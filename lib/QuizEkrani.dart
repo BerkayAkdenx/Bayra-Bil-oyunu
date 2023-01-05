a91e021d0eba945b1cb70fefe73aa49ce2ffc6f5
BerkayAkdenx <ismetberkayakdeniz@gmail.com>
2023-01-05 19:14:58 +0300

    resimler eklendi

---
+++ lib/QuizEkrani.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class QuizEkrani extends StatefulWidget {
+  const QuizEkrani({Key? key}) : super(key: key);
+
+  @override
+  State<QuizEkrani> createState() => _QuizEkraniState();
+}
+
+class _QuizEkraniState extends State<QuizEkrani> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz Ekranı"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[],
+        ),
+      ),
+    );
+    ;
+  }
+}
---
+++ lib/SonucEkrani.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SonucEkrani extends StatefulWidget {
+  const SonucEkrani({Key? key}) : super(key: key);
+
+  @override
+  State<SonucEkrani> createState() => _SonucEkraniState();
+}
+
+class _SonucEkraniState extends State<SonucEkrani> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sonuc Ekrani"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+
+    );;
+  }
+}
